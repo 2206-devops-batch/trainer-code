1152ad1ae44d6cd21dd969ba365c40c3e1a5d17f
Bryon Smith <bryon@smithauto.us>
2022-06-09 15:18:37 -0500

    fix input error

--- 1-python/transpose/transpose.py
+++ 1-python/transpose/transpose.py
@@ -2,31 +2,23 @@ from this import d
 
 
 def transpose(string_input):
-    a = string_input[0]
-    b = string_input[1]
-    print(a)
-    print(b)
+    str_list = string_input.split('\n')
+
     new_list = []
 
-    # a is bigger or same size as b
-    if len(a) >= len(b):
-        for i in range(len(a)):
-            new_list.append(a[i]+b[i])
-        return new_list
+    # find longest line length?
+    longest_str = max(str_list, key=len)
+
+    for i in range(len(longest_str)):
+        output_line = ''
+        for text in str_list:
+            if text[i]:
+                output_line += text[i]
+        new_list.append(output_line)
+
     
-    # b is bigger than a
-    else:
-        for i in range(len(b)):
-            new_list.append(a[i]+b[i])
-        return new_list
+    return '\n'.join(new_list)
 
 
 
-
-
-    # for i in range(len(string_input)):
-    #     a = string_input[i]
-    #     b = string_input
-
-
-print(transpose(['ABC', 'DEF']))
+# print(transpose('ABC\nDdEF'))

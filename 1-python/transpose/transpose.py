24d362ac02ec3acac385ab4f8a691f3cbf4c145c
Bryon Smith <bryon@smithauto.us>
2022-06-09 14:40:12 -0500

    first attempt at transpose

--- 1-python/transpose/transpose.py
+++ 1-python/transpose/transpose.py
@@ -1,6 +1,32 @@
-def transpose(string_input):
-    if string_input == 'A1':
-        return 'A\n1'
-    return 'idk'
+from this import d
 
-# print(transpose([]))
+
+def transpose(string_input):
+    a = string_input[0]
+    b = string_input[1]
+    print(a)
+    print(b)
+    new_list = []
+
+    # a is bigger or same size as b
+    if len(a) >= len(b):
+        for i in range(len(a)):
+            new_list.append(a[i]+b[i])
+        return new_list
+    
+    # b is bigger than a
+    else:
+        for i in range(len(b)):
+            new_list.append(a[i]+b[i])
+        return new_list
+
+
+
+
+
+    # for i in range(len(string_input)):
+    #     a = string_input[i]
+    #     b = string_input
+
+
+print(transpose(['ABC', 'DEF']))

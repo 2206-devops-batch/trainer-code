8faf811c9282e5f565d2615706261caabff757d3
Nick Escalona <nicholas.escalona@gmail.com>
2022-06-09 15:18:19 -0500

    half implementation of transpose

--- 1-python/transpose/transpose.py
+++ 1-python/transpose/transpose.py
@@ -1,6 +1,28 @@
-def transpose(string_input):
-    if string_input == 'A1':
-        return 'A\n1'
-    return 'idk'
+def transpose(lines):
+    input_list = lines.split('\n')  # or splitlines
+    input_height = len(input_list)
+    input_width = get_input_width(input_list)
 
-# print(transpose([]))
+    output_list = []
+
+    for colnum in range(input_width):
+        output = ''
+        for rownum in range(input_height):
+            output += get_char(input_list, rownum, colnum)
+        # output = output.rstrip('*').replace('*', ' ')
+        output_list.append(output)
+
+    return '\n'.join(output_list)
+
+def get_input_width(input_list):
+    return 0
+
+def get_char(input_list, rownum, colnum):
+    return ' '
+
+# print(transpose('ABC\nDEF'))
+# print(transpose('ABC\nDE'))
+# print(transpose('AB\nDE'))
+
+# turn "ABC\nDEF"
+# into: "AD\nBE\nCF"
